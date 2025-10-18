e51023d456e19a35d14817377158cd745ebb13cf
Vishal Kumar <vishalkumar240107@gmail.com>
2025-10-18 20:15:42 +0530

    define the JS

---
+++ app.js
@@ -0,0 +1,57 @@
+let questions = [
+    {
+        words : ['mango', 'banana', 'apple'],
+        options : ['fruits', 'animals'],
+        answer  : 0, 
+    },
+    {
+        words : ['dog', 'cat', 'elephant'],
+        options : ['vehicles', 'animals'],
+        answer  : 1,
+    },
+    {
+        words : ['car', 'bus', 'bicycle'],
+        options : ['vehicles', 'fruits'],
+        answer  : 0,
+    },
+    {
+        words : ['rose', 'tulip', 'daisy'],
+        options : ['flowers', 'vehicles'],
+        answer  : 0,
+    }
+];
+
+const questionSpace = document.getElementById('question-display');
+const scoreSpace = document.getElementById('score');
+
+let score = 0;
+scoreSpace.textContent = score;
+
+function displayQuestions() {
+    questions.forEach( question => {
+        let questionDiv = document.createElement('div');
+        questionDiv.classList.add('question-div');
+        
+        question.words.forEach( questionWord => {
+            let queText = document.createElement('p');
+            queText.textContent = questionWord;
+            questionDiv.append(queText);
+        })
+
+        let optionButtons = document.createElement('div');
+        optionButtons.classList.add('option-buttons');
+        questionDiv.append(optionButtons);
+
+        question.options.forEach( option => {
+            let optionButton = document.createElement('button');
+            optionButton.textContent = option;
+            optionButtons.append(optionButton);
+        })
+
+
+
+
+        questionSpace.append(questionDiv);
+    }) 
+}
+displayQuestions();
